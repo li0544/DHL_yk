2fd7241f75390e169eed46502a394604bc9486e1
li0544 <li0544@163.com>
2015-01-17 09:18:55 +0800

    1.add src;

---
+++ Src/DHL_yk_rename.py
@@ -0,0 +1,58 @@
+# -*- coding: gbk -*-
+'''
+Created on 2014-11-10
+@author: wp
+@note: rename files and call the aspack compression
+'''
+import os
+import re
+import time
+import datetime
+import operator
+
+name = "大灰狼远程管理"
+path = "D:\\Program\\Tem_C\\0001.远控\\0101.大灰狼远控DDos版\\Bin"
+     
+def rename_files():
+    prefix = name + ".exe"
+    for file in os.listdir(path):
+        if os.path.isfile(os.path.join(path,file)):     #判断是否为文件
+            #if re.match(prefix, file):
+            #print(operator.eq(prefix, file))
+            if operator.eq(prefix, file):
+                print("找到文件：" + file)
+                now = datetime.datetime.now()
+                s1 = now.strftime('%Y-%m-%d')   #now.strftime('%Y-%m-%d %H:%M:%S')
+                new_name = name + "_" + s1 + ".exe"
+                bak_file = name + "_" + s1 + ".exe.bak"
+
+                if os.path.isfile(os.path.join(path,new_name)): #判断新文件是否存在
+                    print(new_name + " 已存在")
+                    for i in range(98,122):
+                        new_name = name + "_" + s1 + chr(i) + ".exe"
+                        bak_file = name + "_" + s1 + chr(i) + ".exe.bak"
+                        if not os.path.isfile(os.path.join(path,new_name)):                            
+                            break
+                        else:
+                            print(new_name + '已存在')
+                        
+                    #os.remove(os.path.join(path,new_name))
+                #new_name = re.sub(prefix, "", file)
+                            
+                newfilename = os.path.join(path, new_name)
+                oldfilename = os.path.join(path, file)
+                bakfilename = os.path.join(path, bak_file)
+                print('rename: ' + newfilename)
+                os.rename(oldfilename, newfilename)
+
+                print('compress: ' + newfilename)
+                os.system("aspack " + newfilename)  #call aspack
+
+                #time.sleep(1) #sleep
+                print('delete: ' + bakfilename)
+                os.remove(bakfilename)  #delete bak file
+                if not os.path.isfile(bakfilename):
+                    print('delete bak file success !')
+#if __name_ == '__main__':
+rename_files()
+
---
+++ Src/del_temp_file.bat
@@ -0,0 +1,13 @@
+del /s /q *.pdb
+del /s /q *.ncb
+del /s /q *.ilk
+del /s /q *.idb
+del /s /q *.pch
+del /s /q *.sdf
+del /s /q *.ipch
+del /s *.dmp
+del /s *.tags
+del /s *.bsc
+del /s *.sbc
+del /s *.sbr
+del /s *.obj
